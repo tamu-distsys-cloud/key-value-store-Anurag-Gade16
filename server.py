6ea8dad93c065f4bddcc144e42f03d5a427f8082
Anurag Gade <anurag@Mac.lan>
2025-06-20 19:45:04 -0500

    cleaned up script

--- server.py
+++ server.py
@@ -1,6 +1,3 @@
-### SERVER CODE - ANURAG GADE
-
-
 import logging
 import threading
 from typing import Tuple, Any
@@ -43,7 +40,6 @@ class KVServer:
 
     def Get(self, args: GetArgs):
         with self.mu:
-
             # here finding the server
             if self.server_id is None:
                 for i, server in enumerate(self.cfg.kvservers):
@@ -52,7 +48,6 @@ class KVServer:
                         break
                 if self.server_id is None:
                     return GetReply(self.data.get(args.key, ""))
-
             # responsblity check
             if self.nreplicas > 1:
                 if hasattr(self.cfg, 'running_servers') and self.server_id not in self.cfg.running_servers:
@@ -81,9 +76,7 @@ class KVServer:
         with self.mu:
             if self.server_id is None:
                 for i, server in enumerate(self.cfg.kvservers):
-                    if server is self:
-                        self.server_id = i
-                        break
+                    if server is self: self.server_id = i; break
                 if self.server_id is None:
                     self.data[args.key] = args.value
                     return PutAppendReply(None)
@@ -91,36 +84,25 @@ class KVServer:
             if self.nreplicas > 1:
                 if hasattr(self.cfg, 'running_servers') and self.server_id not in self.cfg.running_servers:
                     return PutAppendReply(None)
-                
-                total_servers = len(self.cfg.kvservers)
-                shard = int(args.key) % total_servers
-                is_responsible = any((shard + i) % total_servers == self.server_id for i in range(self.nreplicas))
-                if not is_responsible:
+                num_servers = len(self.cfg.kvservers)
+                shard = int(args.key) % num_servers
+                if not any((shard + i) % num_servers == self.server_id for i in range(self.nreplicas)):
                     return PutAppendReply(None)
-                
-                # forwarding to primary
-                primary_id = shard
-                if self.server_id != primary_id:
+                if self.server_id != shard:
                     try:
-                        if hasattr(self.cfg, 'kvservers') and primary_id < len(self.cfg.kvservers):
-                            primary_server = self.cfg.kvservers[primary_id]
-                            forwarded_result = primary_server.Put(args)
-                            if forwarded_result is not None:
-                                return forwarded_result
+                        result = self.cfg.kvservers[shard].Put(args)
+                        return result if result else PutAppendReply(None)
                     except:
                         pass
             
-            if hasattr(args, 'client_id') and args.client_id is not None:
-                if args.client_id in self.client_results:
-                    cached_seq, cached_result = self.client_results[args.client_id]
-                    if args.seq_num == cached_seq:
-                        return PutAppendReply(cached_result)
+            if hasattr(args, 'client_id') and args.client_id is not None and args.client_id in self.client_results:
+                old_seq, old_result = self.client_results[args.client_id]
+                if args.seq_num == old_seq:
+                    return PutAppendReply(old_result)
             
             self.data[args.key] = args.value
-            
             if hasattr(args, 'client_id') and args.client_id is not None:
                 self.client_results[args.client_id] = (args.seq_num, None)
-            
             return PutAppendReply(None)
 
     def Append(self, args: PutAppendArgs):
@@ -131,9 +113,9 @@ class KVServer:
                         self.server_id = i
                         break
                 if self.server_id is None:
-                    old_value = self.data.get(args.key, "")
-                    self.data[args.key] = old_value + args.value
-                    return PutAppendReply(old_value)
+                    prev_val = self.data.get(args.key, "")
+                    self.data[args.key] = prev_val + args.value
+                    return PutAppendReply(prev_val)
             
             if self.nreplicas > 1:
                 if hasattr(self.cfg, 'running_servers') and self.server_id not in self.cfg.running_servers:
@@ -141,33 +123,35 @@ class KVServer:
                 
                 total_servers = len(self.cfg.kvservers)
                 shard = int(args.key) % total_servers
-                is_responsible = any((shard + i) % total_servers == self.server_id for i in range(self.nreplicas))
-                if not is_responsible:
+                responsible = False
+                for i in range(self.nreplicas):
+                    if (shard + i) % total_servers == self.server_id:
+                        responsible = True
+                        break
+                if not responsible:
                     return PutAppendReply("")
                 
-                primary_id = shard
-                if self.server_id != primary_id:
+                if self.server_id != shard:
                     try:
-                        if hasattr(self.cfg, 'kvservers') and primary_id < len(self.cfg.kvservers):
-                            primary_server = self.cfg.kvservers[primary_id]
-                            forwarded_result = primary_server.Append(args)
-                            if forwarded_result is not None:
-                                return forwarded_result
+                        primary = self.cfg.kvservers[shard]
+                        result = primary.Append(args)
+                        if result:
+                            return result
                     except:
                         pass
             
-            if hasattr(args, 'client_id') and args.client_id is not None:
+            if hasattr(args, 'client_id') and args.client_id:
                 if args.client_id in self.client_results:
-                    cached_seq, cached_result = self.client_results[args.client_id]
-                    if args.seq_num == cached_seq:
-                        return PutAppendReply(cached_result)
+                    seq, res = self.client_results[args.client_id]
+                    if args.seq_num == seq:
+                        return PutAppendReply(res)
             
-            old_value = self.data.get(args.key, "")
-            self.data[args.key] = old_value + args.value
+            current_val = self.data.get(args.key, "")
+            self.data[args.key] = current_val + args.value
             
-            if hasattr(args, 'client_id') and args.client_id is not None:
-                self.client_results[args.client_id] = (args.seq_num, old_value)
+            if hasattr(args, 'client_id') and args.client_id:
+                self.client_results[args.client_id] = (args.seq_num, current_val)
             
-            return PutAppendReply(old_value)
+            return PutAppendReply(current_val)
         
 # Referred to Perplexity for responsibility checks for keys, and to refine duplicate checking in server.py. Referred perplexity for help to implement sharding checks in client.py
